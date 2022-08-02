7a5083bb27453ab335ab034d61ca41cbc2e2ca03
Somala Prasanna Kumar <104211409+Prasannakumar55@users.noreply.github.com>
2022-08-02 14:18:45 +0530

    Add files via upload

---
+++ Extra.js
@@ -0,0 +1,39 @@
+// 1 . what is Javascript?
+Javascript is a synchronous single threaded language 
+which can use dynamically of website.
+Javascript interact with the browser and used the 
+both on the client-side and service side. 
+
+// 2 . what is the javascript run time environment?
+The JavaScript runtime environment provides access 
+to built-in libraries and objects that are available 
+to a program so that it can interact with the 
+outside world and make the code work.
+It is built on a single-threaded, 
+non-blocking Event loop, 
+the Google Chrome V8 JavaScript engine, 
+Web Apis,
+
+// 3 . what is execution context?
+Everything in javascript happend inside the 
+"execution constant".
+it have two components 
+    (i) . Memory
+    (ii). code
+    it first allocated in the Memory as a variables and function
+    then it will executive the part of the code from line by line
+    because its a single threaded.
+    the execution context is a whole "environment" where the
+    function execution and contains variable scope. 
+
+// 4 . what is javascript Engine?
+The JavaScript Engine is used to run the JavaScript code in 
+the browser. Every Browser have their own JavaScript Engine to run.
+Chrome have : V8,
+Mozilla have : spider Monkey
+internet Explorer have : Chakra
+
+// . flow of data in engine?
+A data flow is a workflow of data processing. 
+Any system where the data moves between code units to 
+execution of the code could be called dataflow.
